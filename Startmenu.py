a1d0e9b8c8d9551226b8cca858e1038c6f0d1849
rus <rus.rifat1@gmail.com>
2020-04-06 19:55:43 -0400

    made menu into a function

--- Startmenu.py
+++ Startmenu.py
@@ -1,38 +1,45 @@
-import tkinter as tk
-from tkinter import Text
-import tkinter.font as tkFont
-import os
+def startMenu():
 
-root = tk.Tk()
-root.title("Amazing Maze")
+    import tkinter as tk
+    from tkinter import Text
+    import tkinter.font as tkFont
+    import os
 
-#the background image path
-background_image = tk.PhotoImage(file = "src/pic2.png")
+    root = tk.Tk()
+    root.title("Amazing Maze")
 
-canvas = tk.Canvas(root, height = 900, width = 900)
-background_label = tk.Label(root, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    #the background image path
+    background_image = tk.PhotoImage(file = "src/maze.png")
 
-canvas.grid()
+    canvas = tk.Canvas(root, height = 900, width = 900)
+    background_label = tk.Label(root, image=background_image)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-fontStyle = tkFont.Font(family="Lucida Grande", size=40)
-gameLabel = tk.Label(root, bg = 'white', text="Amazing Maze", font = fontStyle)
-gameLabel.place(x = 250, y = 0)
+    canvas.grid()
 
-startButton = tk.Button(root, bg="white", bd = 3, text = "Start game", padx = 30, pady = 20)
-startButton.place(x = 300, y =700)
+    fontStyle = tkFont.Font(family="Lucida Grande", size=40)
+    gameLabel = tk.Label(root, bg = 'white', text="Amazing Maze", font = fontStyle)
+    gameLabel.place(x = 250, y = 0)
 
-loadButton = tk.Button(root, bg = 'white', bd = 3, text = "Load game", padx = 30, pady = 20)
-loadButton.place(x = 440, y = 700)
+    startButton = tk.Button(root, bg="white", bd = 3, text = "Start game", padx = 30, pady = 20)
+    startButton.place(x = 250, y =750)
 
-difficultyButton = tk.Menubutton(root, bg = "white", bd = 3, text="Diffifulty", padx = 30, pady = 20)
-difficultyButton.place(x=580, y =700)
+    loadButton = tk.Button(root, bg = 'white', bd = 3, text = "Load game", padx = 30, pady = 20)
+    loadButton.place(x = 390, y = 750)
 
-difficultyButton.menu =  tk.Menu ( difficultyButton, tearoff = 0 )
-difficultyButton["menu"] =  difficultyButton.menu
+    difficultyButton = tk.Menubutton(root, bg = "white", bd = 3, text="Diffifulty", padx = 30, pady = 20)
+    difficultyButton.place(x=530, y =750)
 
-difficultyButton.menu.add_checkbutton ( label="easy", variable=tk.IntVar() )
-difficultyButton.menu.add_checkbutton ( label="medium", variable=tk.IntVar() )
-difficultyButton.menu.add_checkbutton ( label="hard", variable=tk.IntVar() )
+    difficultyButton.menu =  tk.Menu ( difficultyButton, tearoff = 0 )
+    difficultyButton["menu"] =  difficultyButton.menu
 
-root.mainloop()
+    difficultyButton.menu.add_checkbutton (label="easy", variable=tk.IntVar())
+    difficultyButton.menu.add_checkbutton (label="medium", variable=tk.IntVar())
+    difficultyButton.menu.add_checkbutton (label="hard", variable=tk.IntVar())
+    difficultyButton.menu.add_checkbutton (label="insane", variable=tk.IntVar())
+
+    root.mainloop()
+
+
+if __name__ == "__main__": 
+    startMenu()
