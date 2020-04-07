b8343d4a3b2ad4846f2f770ebf0fe439fe6809d8
zyang37 <zhenningyang0139@gmail.com>
2020-04-07 00:35:51 -0400

    updated

--- Startmenu.py
+++ Startmenu.py
@@ -1,51 +1,68 @@
+import tkinter as tk
+from tkinter import *
+#from tkinter import Text
+import tkinter.font as tkFont
+from PIL import Image, ImageTk
+import os
+
 def start(gameStat):
     gameStat[0] = 1
 
 def startMenu():
-
-    import tkinter as tk
-    from tkinter import Text
-    import tkinter.font as tkFont
-    import os
 
     gameStat = [0]
 
     root = tk.Tk()
     root.title("Amazing Maze")
 
-    #the background image path
-    background_image = tk.PhotoImage(file = "backgrounds/maze.png")
-
     canvas = tk.Canvas(root, height = 900, width = 900)
+    canvas.pack(fill=tk.BOTH, expand=tk.YES)
+
+    background_image = tk.PhotoImage(file = "backgrounds/maze.png")
     background_label = tk.Label(root, image=background_image)
-    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    background_label.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
     canvas.grid()
 
-    fontStyle = tkFont.Font(family="Lucida Grande", size=40)
+    fontStyle = tkFont.Font(family="Lucida Grande", size=50)
     gameLabel = tk.Label(root, bg = 'white', text="Amazing Maze", font = fontStyle)
-    gameLabel.place(x = 250, y = 0)
+    gameLabel.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-    startButton = tk.Button(root, bg="white", bd = 3, text = "Start game", command = lambda:[start(gameStat), root.destroy()], padx = 30, pady = 20)
-    startButton.place(x = 250, y =750)
+    startButton = tk.Button(root, bg="white", bd = 3, text = "Start game", command = lambda:[start(gameStat), root.destroy()], padx = 50, pady = 30)
+    startButton.place(relx=0.25, rely=0.89, anchor=CENTER)
 
-    loadButton = tk.Button(root, bg = 'white', bd = 3, text = "Load game", padx = 30, pady = 20)
-    loadButton.place(x = 390, y = 750)
+    loadButton = tk.Button(root, bg = 'white', bd = 3, text = "Load game", padx = 50, pady = 30)
+    loadButton.place(relx=0.5, rely=0.89, anchor=CENTER)
 
-    difficultyButton = tk.Menubutton(root, bg = "white", bd = 3, text="Diffifulty", padx = 30, pady = 20)
-    difficultyButton.place(x=530, y =750)
+    # Create a Tkinter variable
+    tkvar = StringVar(root)
+    tkvar.set('medium') # set the default option
+    popupMenu = OptionMenu(canvas, tkvar, 'easy','medium','hard','insane')
+    popupMenu.config(width=15)
+    popupMenu.place(relx=0.75, rely=0.84, anchor=CENTER)
 
+    # on change dropdown value
+    def change_dropdown(*args):
+        print(tkvar.get())
+    # link function to change dropdown
+    tkvar.trace('w', change_dropdown)
+
+    '''
+    difficultyButton = tk.Menubutton(root, bg = "white", bd = 3, text="Diffifulty", padx = 50, pady = 30)
+    difficultyButton.place(relx=0.75, rely=0.89, anchor=CENTER)
     difficultyButton.menu =  tk.Menu ( difficultyButton, tearoff = 0 )
     difficultyButton["menu"] =  difficultyButton.menu
-
     difficultyButton.menu.add_checkbutton (label="easy", variable=tk.IntVar())
     difficultyButton.menu.add_checkbutton (label="medium", variable=tk.IntVar())
     difficultyButton.menu.add_checkbutton (label="hard", variable=tk.IntVar())
     difficultyButton.menu.add_checkbutton (label="insane", variable=tk.IntVar())
+    '''
 
     root.mainloop()
 
-    return gameStat
+    return gameStat, tkvar.get()
 
-if __name__ == "__main__": 
-    startMenu()
+if __name__ == "__main__":
+    stat, level = startMenu()
+    print(stat)
+    print(level)
