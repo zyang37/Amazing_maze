52a611b48d292b4a87aaf5442b13e44b4310c043
zyang37 <zhenningyang0139@gmail.com>
2020-04-11 13:51:42 -0400

    gameover and win

--- start.py
+++ start.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 from maze_gen import *
 from helper import *
 from Startmenu import startMenu
+import time
+import sys
 
 # helper functions
 def find_random_spot(listmaze):
@@ -77,15 +79,18 @@ maze = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 def main(level, enemies):
     global score, BASICFONT, FPSCLOCK, maze, resolution
+
     pygame.init()
 
     icon = pygame.image.load('img/icon.png')
     star = pygame.image.load('img/star.png')
     enemy = pygame.image.load('img/enemy.png')
     playerImg = pygame.image.load('img/mushroom.png')
+    exitlogo = pygame.image.load('img/exit.png')
     playerImg = pygame.transform.scale(playerImg, object_size)
     star = pygame.transform.scale(star,object_size)
     enemy = pygame.transform.scale(enemy,object_size)
+    exitlogo = pygame.transform.scale(exitlogo,object_size)
     pygame.display.set_icon(icon)
 
     FPSCLOCK = pygame.time.Clock()
@@ -94,9 +99,22 @@ def main(level, enemies):
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     pygame.display.set_caption("Amazing Maze")
     star_store = (find_random_spot(maze))
+
+
+    exit_location = [len(maze)-1,len(maze[0])-2]
+    #print(len(maze), len(maze[0]))
+    #print(exit_location)
+    over = 0
+    win = 0
+
     while True:
         for event in pygame.event.get():
-            if event.type == KEYDOWN:
+            if event.type == KEYDOWN and (win or over):
+                print("key press!")
+                time.sleep(2)
+                pygame.quit()
+                return
+            elif event.type == KEYDOWN:
                 key = event.key
                 if key == K_UP:
                     move(0, -1)
@@ -107,27 +125,59 @@ def main(level, enemies):
                 elif key == K_LEFT:
                     move(-1, 0)
             elif event.type == QUIT:
-                return
+                pygame.quit()
+                sys.exit()
+                #return
+
+        # star
         if current_position==star_store:
             star_store = (find_random_spot(maze))
             delete_random_wall(maze)
             score+=1
-
         draw_maze(screen)
         draw_player(playerImg, screen)
         draw_star(star,screen,star_store)
-        # test
+        drawScore(score,screen)
+
+        draw_star(exitlogo, screen, exit_location)
+
+        # display enemies
         for e in enemies:
             enemy_position = e
             enemy_position = move_enemy(enemy_position)
             draw_star(enemy,screen,enemy_position)
-        drawScore(score,screen)
+
+        if current_position in enemies:
+            if not over:
+                print("GAME OVER\n")
+            over = 1
+
+        if current_position == exit_location:
+            if not win:
+                print("WIN\n")
+            win = 1
+        # gameover or win
+        if over:
+            fontB = pygame.font.SysFont("comicsansms", 150)
+            fontS = pygame.font.SysFont("comicsansms", 45)
+            GAMEOVER = fontB.render("GAME OVER!!!", True, (255,127,80))
+            ANYKEYS = fontS.render("Press any key to continue", True, (0, 128, 0))
+            screen.blit(GAMEOVER,(resolution[0]/2 - GAMEOVER.get_width() // 2, resolution[0]/2.5 - GAMEOVER.get_height() // 2))
+            screen.blit(ANYKEYS,(resolution[0]/2 - ANYKEYS.get_width() // 2, resolution[0]/2 - ANYKEYS.get_height() // 2))
+        elif win:
+            fontB = pygame.font.SysFont("comicsansms", 150)
+            fontS = pygame.font.SysFont("comicsansms", 45)
+            WIN = fontB.render("GOOD JOB!!!", True, (255,127,80))
+            ANYKEYS = fontS.render("Press any key to continue", True, (0, 128, 0))
+            screen.blit(WIN,(resolution[0]/2 - WIN.get_width() // 2, resolution[0]/2.5 - WIN.get_height() // 2))
+            screen.blit(ANYKEYS,(resolution[0]/2 - ANYKEYS.get_width() // 2, resolution[0]/2 - ANYKEYS.get_height() // 2))
+
         pygame.display.update()
         FPSCLOCK.tick(45)
 
 
 if __name__ == "__main__":
-
+    '''
     # Game loop
     stat, level = startMenu()
     print(stat[0])
@@ -176,5 +226,64 @@ if __name__ == "__main__":
             enemies.append(find_random_spot(maze))
 
         main(level, enemies)
+    '''
+    #levels = ['easy', 'medium', 'hard', 'insane']
+    #stat = [0]
+    #level = levels[1]
+    #win = 0
+
+    while 1:
+        #if win==0:
+        stat, level = startMenu()
+
+        if stat[0]:
+            # maze
+            resolution = (850, 850)
+            current_position = [0, 1]
+            score = 0
+            enemy_num = 0
+            enemies = []
+
+            #if win and level!="insane":
+            #    level = levels[levels.index(level) + 1]
+
+            if level=='easy':
+                maze = maze_gen(6, 6)
+                object_size = (37, 37)
+                cell_margin = 0.5
+                # pink white
+                cell_colors = (255, 192, 203), (255, 255, 255)
+            elif level=='medium':
+                maze = maze_gen(10, 10)
+                object_size = (30, 30)
+                cell_margin = 0.5
+                # green white
+                cell_colors = (144,238,144),(255, 255, 255)
+                #enemy_position = find_random_spot(maze)
+                enemy_num = 1
+            elif level=='hard':
+                maze = maze_gen(18, 18)
+                object_size = (20, 20)
+                cell_margin = 0.5
+                # blue white
+                cell_colors = (0,191,255), (255, 255, 255)
+                #enemy_position = find_random_spot(maze)
+                enemy_num = 2
+            elif level=='insane':
+                maze = maze_gen(26, 26)
+                object_size = (15, 15)
+                cell_margin = 0.5
+                # red white
+                cell_colors = (220,20,60), (169,169,169)
+                enemy_num = 5
+
+            for i in range(enemy_num):
+                #print(i)
+                enemies.append(find_random_spot(maze))
+
+            main(level, enemies)
+        else:
+            break
 
     pygame.quit()
+    sys.exit()
